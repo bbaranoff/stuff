50677839979c52f75b413ee6ed5f5dd265539e3a
nirvana <admin@pl4y.store>
2020-05-16 22:49:42 +0200

    commit

--- catch_rand.c
+++ catch_rand.c
@@ -2,7 +2,9 @@
 void main(void){
 char *test;
 test=catch_rand();
-printf("test %s",test);
+printf("test %s\n",test);
 char *rand=strtok(test,"-");
-printf("rand %s",rand);
+printf("rand %s\n",rand);
+char *tmp=strtok(NULL,"-");
+printf("key_seq %s\n",tmp);
 }
